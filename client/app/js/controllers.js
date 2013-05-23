9d13292e898cf9a0f731d21e637ad3bc2ea82f62
Matthieu Napoli <matthieu@mnapoli.fr>
2013-05-23 23:49:56 +0200

    Changed URL

--- client/app/js/controllers.js
+++ client/app/js/controllers.js
@@ -5,7 +5,7 @@
 angular.module('wikt.controllers', ['ng']).
     controller('DocumentController', [function ($scope, $ng) {
         $scope.getSample = function() {
-            $ng.http({method: 'GET', url: '/'})
+            $ng.http({method: 'GET', url: 'http://localhost:12639/file/README.md'})
                 .success(function (data, status, headers, config) {
                     $scope.documentContent = data;
                     $scope.view = './views/document/sample'
