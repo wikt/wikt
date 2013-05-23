af8311c7e75a4f69ffcd1dfd211d874bc4e298a5
Matthieu Napoli <matthieu@mnapoli.fr>
2013-05-23 23:35:41 +0200

    Added POST on /file/:name resource

--- server/server.js
+++ server/server.js
@@ -9,13 +9,15 @@ var repo = git(repositoryDirectory);
 // REST server
 var restify = require('restify');
 
+var server = restify.createServer();
+
 /**
  * File controller
  * @constructor
  */
 var FileController = function() {
     /**
-     * GET
+     * GET: returns file content
      */
     this.get = function(req, res, next) {
         console.log('Sending file ' + req.params.name);
@@ -25,12 +27,21 @@ var FileController = function() {
 
             res.send('hello ' + data);
         });
-    }
+    };
+    /**
+     * POST: edit file content
+     */
+    this.post = function(req, res, next) {
+        console.log('Modifying file ' + req.params.name);
+
+        fs.writeFile(repositoryDirectory + '/' + req.params.name, req.params.content, function(err) {
+            if (err) throw err;
+        });
+    };
 };
 var fileController = new FileController();
-
-var server = restify.createServer();
 server.get('/file/:name', fileController.get);
+server.post('/file/:name', fileController.post);
 
 server.listen(12639, function() {
     console.log('%s listening at %s', server.name, server.url);
