c7a05e98b926223ff01a9c4913180ee61e59a4bc
Matthieu Napoli <matthieu@mnapoli.fr>
2013-05-24 00:00:18 +0200

    Separated File controller to a module

--- server/server.js
+++ server/server.js
@@ -6,38 +6,10 @@ var repo = git(config.repositoryDirectory);
 
 // REST server
 var restify = require('restify');
-
 var server = restify.createServer();
 
-/**
- * File controller
- * @constructor
- */
-var FileController = function() {
-    /**
-     * GET: returns file content
-     */
-    this.get = function(req, res, next) {
-        console.log('Sending file ' + req.params.name);
-
-        fs.readFile(config.repositoryDirectory + '/' + req.params.name, 'utf8', function(err, data) {
-            if (err) throw err;
-
-            res.send('hello ' + data);
-        });
-    };
-    /**
-     * POST: edit file content
-     */
-    this.post = function(req, res, next) {
-        console.log('Modifying file ' + req.params.name);
-
-        fs.writeFile(config.repositoryDirectory + '/' + req.params.name, req.params.content, function(err) {
-            if (err) throw err;
-        });
-    };
-};
-var fileController = new FileController();
+var FileController = require('./controllers/file');
+var fileController = new FileController(config);
 server.get('/file/:name', fileController.get);
 server.post('/file/:name', fileController.post);
 
