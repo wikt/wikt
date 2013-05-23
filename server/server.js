bdfd3d4928ecc3df210cb384676704a49eee2d1e
Matthieu Napoli <matthieu@mnapoli.fr>
2013-05-23 23:30:55 +0200

    Added controllers for server

--- server/server.js
+++ server/server.js
@@ -9,18 +9,28 @@ var repo = git(repositoryDirectory);
 // REST server
 var restify = require('restify');
 
-function respond(req, res, next) {
-    console.log('Sending file ' + req.params.name);
+/**
+ * File controller
+ * @constructor
+ */
+var FileController = function() {
+    /**
+     * GET
+     */
+    this.get = function(req, res, next) {
+        console.log('Sending file ' + req.params.name);
 
-    fs.readFile(repositoryDirectory + '/' + req.params.name, 'utf8', function(err, data) {
-        if (err) throw err;
+        fs.readFile(repositoryDirectory + '/' + req.params.name, 'utf8', function(err, data) {
+            if (err) throw err;
 
-        res.send('hello ' + data);
-    });
-}
+            res.send('hello ' + data);
+        });
+    }
+};
+var fileController = new FileController();
 
 var server = restify.createServer();
-server.get('/file/:name', respond);
+server.get('/file/:name', fileController.get);
 
 server.listen(12639, function() {
     console.log('%s listening at %s', server.name, server.url);
