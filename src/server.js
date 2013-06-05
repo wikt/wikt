44b65860218635160210d46acc130f4bf0b87fec
Matthieu Napoli <matthieu@mnapoli.fr>
2013-06-05 20:06:28 +0200

    Reorganization of the folders, and switched to Express as HTTP server library

---
+++ src/server.js
@@ -0,0 +1,30 @@
+var config = require('./config');
+var git = require('gift');
+var fs = require('fs');
+
+var repo = git(config.repositoryDirectory);
+
+// Server
+var express = require('express');
+var app = express();
+
+app.use(express.logger());
+
+// DirectoryController
+var DirectoryController = require('./controllers/file');
+var directoryController = new DirectoryController(config);
+app.get('/directory/:name', directoryController.get);
+
+// FileController
+var FileController = require('./controllers/file');
+var fileController = new FileController(config);
+app.get('/file/:name', fileController.get);
+app.post('/file/:name', fileController.post);
+
+
+// Serve static files of the JS app
+app.use('/', express.static(__dirname + '/public'));
+
+app.listen(8080, function() {
+    console.log("Listening on port 8080");
+});
