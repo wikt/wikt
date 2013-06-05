e0a2d54e54ae2b5c646a23acd9da341f3719fa2d
Xavier Lucas <xavier.lucas.2@gmail.com>
2013-06-05 21:57:56 +0200

    Better structure for the client application

--- src/public/js/apps/wikt.js
+++ src/public/js/apps/wikt.js
@@ -4,6 +4,6 @@
 // Declare app level module which depends on filters, and services
 angular.module('wikt', ['wikt.filters', 'wikt.services', 'wikt.directives', 'wikt.controllers']).
     config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/sample', {templateUrl:'views/document/sample.html', controller:'DocumentController.js'});
+    $routeProvider.when('/sample', {templateUrl:'views/document/sample.html', controller:'DocumentController'});
     $routeProvider.otherwise({redirectTo: '/sample'});
 }]);
